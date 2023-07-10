ebf0cdd24b533da841d8909a1078fbe5c6d50d02
Lungeanu Razvan <razvanlungeanu21@gmail.com>
2023-07-10 16:48:51 +0300

    changed repository

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# MoX
-Movies and TV Shows Explorer
+# WEBProject
+
+Proiect realizat de:
+Ghiorghi Ioana I2A2,
+Lungeanu Razvan I2A3,
+Lupu Cosmin-Daniel I2A2
---
+++ src/api/app/src/endpoints/index.ts
@@ -0,0 +1,39 @@
+import { Endpoint } from "./Endpoint";
+import { HomeEndpoint } from "./HomeEndpoint";
+import { NotFoundEndpoint } from "./NotFoundEndpoint";
+import { RegisterEndpoint } from "./RegisterEndpoint";
+import { LoginEndpoint } from "./LoginEndpoint";
+import { MoviesEndpoint } from "./MoviesEndpoint";
+import { DetailsEndpoint } from "./DetailsEndpoint";
+import { GenreEndpoint } from "./GenreEndpoint";
+import { StaticEndpoint } from "./StaticEndpoint";
+import { ListEndpoint } from "./ListEndpoint";
+import { AdminEndpoint } from "./AdminEndpoint";
+import { ProfileEndpoint } from "./ProfileEndpoint";
+import { AboutEndpoint } from "./AboutEndpoint";
+import { StatsEndpoint } from "./StatsEndpoint";
+import { ChartEndpoint } from "./ChartEndpoint";
+import { OverviewEndpoint } from "./OverviewEndpoint";
+import { VerifyEndpoint } from "./VerifyEndpoint";
+import { LogoutEndpoint } from "./LogoutEndpoint";
+
+export {
+  Endpoint,
+  HomeEndpoint,
+  NotFoundEndpoint,
+  RegisterEndpoint,
+  LoginEndpoint,
+  MoviesEndpoint,
+  DetailsEndpoint,
+  GenreEndpoint,
+  StaticEndpoint,
+  ListEndpoint,
+  AdminEndpoint,
+  ProfileEndpoint,
+  AboutEndpoint,
+  StatsEndpoint,
+  ChartEndpoint,
+  OverviewEndpoint,
+  VerifyEndpoint,
+  LogoutEndpoint,
+};
